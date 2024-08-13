646022b74d08a935758d37e6bc55b272404d2b05
= <=>
2024-08-13 13:31:20 -0300

    code re structure

--- electron/Tray.js
+++ electron/Tray.js
@@ -1,20 +1,21 @@
-const { Menu, Tray, app } = require('electron');
-const menuTemplate = require('./menuTemplate/template');
-const createWindow = require('./BrowserWindow');
+const { Menu, Tray } = require('electron');
 const { resolve } = require('path');
-const windowPositioner = require('./windowPositioner');
+const icon = resolve(__dirname, '../', 'assets', 'icon.png');
 
 function createTray () {
-  const tray = new Tray(resolve(__dirname, '../', 'assets', 'icon.png'));
-  const contextMenu = Menu.buildFromTemplate(menuTemplate);
+  const tray = new Tray(icon);
   tray.setToolTip('Tray Tools');
 
+  const createWindow = require('./BrowserWindow');
   const win = createWindow();
+
   tray.on('click', () => {
-    // windowPositioner(tray, win);
+    const windowPositioner = require('./windowPositioner');
+    windowPositioner(tray, win);
   });
 
   tray.on('right-click', () => {
+    const contextMenu = Menu.buildFromTemplate(require('./menuTemplate/template'));
     tray.setContextMenu(contextMenu);
     tray.popUpContextMenu();
   });
