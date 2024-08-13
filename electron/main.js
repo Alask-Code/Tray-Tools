76b4da445a9273f2ce9173890ccb5d101725e115
= <=>
2024-08-13 15:30:16 -0300

    this is not working for some reason

--- electron/main.js
+++ electron/main.js
@@ -2,7 +2,9 @@ require('electron-reload')('../');
 const { app } = require('electron');
 const createTray = require('./Tray');
 const { appName } = require('../manifest.json');
+
 app.whenReady().then(() => {
   const tray = createTray();
 });
+
 app.setAppUserModelId(appName);
